27f5da309b3e10a65576d81c2fb9d38c56ec6200
packleader206 <packleader206@gmail.com>
2019-03-15 15:01:21 -0700

    set up routes, mongodb atlas api key, passport

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://packleader206:aftermath@71!@project3-2w12c.mongodb.net/test?retryWrites=true" 
+    mongoURI: "mongodb+srv://packleader206:aftermath@71!@project3-2w12c.mongodb.net/test?retryWrites=true",
+    secretOrKey: "XKRMVICH" 
   };
