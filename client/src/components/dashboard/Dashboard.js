ad4e267241e9853bab6835721d5f62974f6f1713
packleader206 <packleader206@gmail.com>
2019-03-17 09:00:47 -0700

    server side git ignore created

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -19,10 +19,9 @@ class Dashboard extends Component {
         <div className="row">
           <div className="landing-copy col s12 center-align">
             <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
+              <b>Hi,</b> {user.name.split(" ")[0]}
               <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
+                You are now logged in!
               </p>
             </h4>
             <button
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: "mongodb+srv://packleader206:aftermath@71!@project3-2w12c.mongodb.net/test?retryWrites=true",
     secretOrKey: "XKRMVICH" 
+    // mongoURI: process.env.MONGODB_URI || "mongodb://localhost/project3"
   };
